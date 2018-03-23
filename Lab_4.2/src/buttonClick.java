a7c5562e6433d23904ee740ff747bc8284d9aea0
BT_1N3_22 <BT_1N3_22@BT_1N3_22.bktech.local>
2018-03-23 09:30:41 -0400

    segsd

---
+++ Lab_4.2/src/buttonClick.java
@@ -0,0 +1,42 @@
+
+import java.awt.Label;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+ 
+public class buttonClick extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Hello World!");
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+        private int counter = 0;
+        private Label label = new Label("Count: ");
+ //sdgd
+            @Override
+            public void handle(ActionEvent event) {
+            	label.setText("pressed:" + counter +"time(s)");        
+        		System.out.println("Hello World!");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+    }
+    public void counter()
+    {
+    	counter++;
+    }
+}
